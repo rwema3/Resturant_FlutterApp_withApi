ad7b3d04dac0afb8f9c9024a06905e3070a0cae8
rwema3 <rwemadominik3@gmail.com>
2022-01-04 19:52:03 +0200

    extends

--- lib/pages/vi.dart
+++ lib/pages/vi.dart
@@ -11,6 +11,7 @@ class Vi extends StatefulWidget {
   @override
   State<Vi> createState() => _ViState();
 }
+
 class _ViState extends State<Vi> {
   @override
   Widget build(BuildContext context) {
