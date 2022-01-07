95e657221055870e9cb15b9c189289f2e5de519f
rwema3 <rwemadominik3@gmail.com>
2022-01-07 16:04:11 +0200

    SharedPreference

--- lib/helper/authenticate.dart
+++ lib/helper/authenticate.dart
@@ -1,5 +1,7 @@
 import 'package:rwemaapi/views/signin.dart';
+
 import 'package:rwemaapi/views/signup.dart';
+
 import 'package:flutter/material.dart';
 
 class Authenticate extends StatefulWidget {
