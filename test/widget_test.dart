3c446fd9af207888abb40b4528394c0ecefe1c8a
rwema3 <rwemadominik3@gmail.com>
2022-01-07 16:20:51 +0200

    firebase auth connection & api

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@ void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
-
+//The following line prevents//The following line prevents
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
