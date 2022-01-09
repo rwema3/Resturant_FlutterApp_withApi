1faf3a5d4b3f94f25f94f4828b8b62ff056c3a03
rwema3 <rwemadominik3@gmail.com>
2022-01-09 20:51:21 +0200

    firebase auth connection & api

--- lib/views/forgot_password.dart
+++ lib/views/forgot_password.dart
@@ -4,7 +4,6 @@ class ForgotPassword extends StatefulWidget {
   @override
   _ForgotPasswordState createState() => _ForgotPasswordState();
 }
-
 class _ForgotPasswordState extends State<ForgotPassword> {
   @override
   Widget build(BuildContext context) {
