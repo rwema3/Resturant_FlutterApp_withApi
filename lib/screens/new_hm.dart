6e959e92d9ac6a2ab60bbad7bc1d507ac968f610
rwema3 <rwemadominik3@gmail.com>
2022-01-09 21:14:23 +0200

    firebase auth connection & api

--- lib/screens/new_hm.dart
+++ lib/screens/new_hm.dart
@@ -37,7 +37,7 @@ class _WebViewAppState extends State<WebViewApp> {
               colors: [ const Color(0xff07172f),
                 const Color(0xff338bbb)],
             ),
-          ),
+          ),//if the login form is submitted
         ),
         leading: Icon(Icons.restaurant),
         title: Text('Rwema & Robert Resto'),
