42fe19f3375f019cbff3e6b4c200c3233a7c9105
rwema3 <rwemadominik3@gmail.com>
2022-01-11 12:10:57 +0200

    firebase auth connection & api

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ void main() {
   runApp(MyApp());
 }
 class MyApp extends StatefulWidget {
-  // This widget is the root of your application.
   @override
   _MyAppState createState() => _MyAppState();
 }
