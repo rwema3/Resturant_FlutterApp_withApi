1a9aaadae373ede1c7808a8860ac8b6d86b8b1b8
rwema3 <rwemadominik3@gmail.com>
2022-01-11 12:02:53 +0200

    firebase auth connection & api

--- lib/helper/authenticate.dart
+++ lib/helper/authenticate.dart
@@ -9,9 +9,9 @@ class Authenticate extends StatefulWidget {
   _AuthenticateState createState() => _AuthenticateState();
 }
 
+
 class _AuthenticateState extends State<Authenticate> {
   bool showSignIn = true;
-
   void toggleView() {
     setState(() {
       showSignIn = !showSignIn;
