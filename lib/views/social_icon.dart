a7ba4804fcc3b663a3d1803c8a9d37be99f3272a
rwema3 <rwemadominik3@gmail.com>
2022-01-11 12:05:21 +0200

    firebase auth connection & api

--- lib/views/social_icon.dart
+++ lib/views/social_icon.dart
@@ -8,7 +8,7 @@ class SocalIcon extends StatelessWidget {
     this.iconSrc,
     this.press,
   }) : super(key: key);
-//if the login f
+//if the login
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
