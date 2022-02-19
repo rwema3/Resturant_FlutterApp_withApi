607091da2d120b9fbf35922ade1c947f61b7cf9a
Rwema <52289151+rwema3@users.noreply.github.com>
2022-02-19 23:30:21 +0200

    gr

--- lib/views/or_divider.dart
+++ lib/views/or_divider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//The flowing lpvents//The following line prevents
+//The fling lpvents//The following line prevents
 class OrDivider extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
